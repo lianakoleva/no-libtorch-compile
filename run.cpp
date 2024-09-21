2f36267916a00c70931e976550449888573f8bbe
liana <43767763+lianakoleva@users.noreply.github.com>
2024-09-21 22:52:15 +0000

    attempting to resolve aoti_torch_device_type_cuda

--- run.cpp
+++ run.cpp
@@ -2,8 +2,15 @@
 #include <dlfcn.h>
 #include <stdlib.h>
 
+extern "C" {
+  int32_t aoti_torch_device_type_cuda() {
+    return 1;
+  }
+}
+
 int main() {
-  void *handle = dlopen("foo.so", RTLD_NOW);
+  dlopen(NULL, RTLD_GLOBAL);
+  void *handle = dlopen("foo.so", RTLD_NOW | RTLD_GLOBAL);
   if (handle == NULL) {
     fprintf(stderr, "Failed to open shared library: %s\n", dlerror());
     exit(1);
