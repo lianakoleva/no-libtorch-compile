100ead516098c0bb16605f538fb0dcac2019d690
liana <43767763+lianakoleva@users.noreply.github.com>
2024-09-21 23:14:23 +0000

    skeleton funcs

--- run.cpp
+++ run.cpp
@@ -1,16 +1,66 @@
 #include <stdio.h>
 #include <dlfcn.h>
 #include <stdlib.h>
+#include <string>
 
 extern "C" {
   int32_t aoti_torch_device_type_cuda() {
     return 1;
   }
-}
-
-extern "C" {
   int32_t aoti_torch_grad_mode_is_enabled() {
     return false;
+  }
+  void aoti_torch_grad_mode_set_enabled(bool enabled) {
+    std::string msg = std::string(__func__) + " DNE";
+    perror(msg.c_str());
+  }
+  void aoti_torch_get_data_ptr() {
+    std::string msg = std::string(__func__) + " DNE";
+    perror(msg.c_str());
+  }
+  void aoti_torch_get_storage_offset() {
+    std::string msg = std::string(__func__) + " DNE";
+    perror(msg.c_str());
+  }
+  void aoti_torch_get_strides() {
+    std::string msg = std::string(__func__) + " DNE";
+    perror(msg.c_str());
+  }
+  void aoti_torch_create_tensor_from_blob_v2() {
+    std::string msg = std::string(__func__) + " DNE";
+    perror(msg.c_str());
+  }
+  void aoti_torch_delete_cuda_stream_guard() {
+    std::string msg = std::string(__func__) + " DNE";
+    perror(msg.c_str());
+  }
+  void aoti_torch_device_type_cpu() {
+    std::string msg = std::string(__func__) + " DNE";
+    perror(msg.c_str());
+  }
+  void aoti_torch_delete_tensor_object() {
+    std::string msg = std::string(__func__) + " DNE";
+    perror(msg.c_str());
+  }
+  void aoti_torch_get_storage_size() {
+    std::string msg = std::string(__func__) + " DNE";
+    perror(msg.c_str());
+  }
+  void aoti_torch_create_cuda_stream_guard() {
+    std::string msg = std::string(__func__) + " DNE";
+    perror(msg.c_str());
+  }
+  void aoti_torch_create_tensor_from_blob() {
+    std::string msg = std::string(__func__) + " DNE";
+    perror(msg.c_str());
+  }
+  void aoti_torch_dtype_float32() {
+    std::string msg = std::string(__func__) + " DNE";
+    perror(msg.c_str());
+  }
+  void aoti_torch_empty_strided() {
+    std::string msg = std::string(__func__) + " DNE";
+    perror(msg.c_str());
   }
 }
 
