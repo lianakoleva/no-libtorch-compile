f01d7094ab56bb34bc319399810198760677e716
albanD <desmaison.alban@gmail.com>
2024-09-21 20:17:54 -0400

    signatures

--- run.cpp
+++ run.cpp
@@ -42,34 +42,56 @@ extern "C" {
     void** ret_data_ptr) {
     *ret_data_ptr = tensor.data_ptr;
   }
-  void aoti_torch_get_storage_offset() {
+  AOTITorchError aoti_torch_get_storage_offset(
+    AtenTensorHandle tensor,
+    int64_t* ret_storage_offset) {
     std::string msg = std::string(__func__) + " DNE";
     perror(msg.c_str());
   }
-  void aoti_torch_get_strides() {
+  AOTITorchError aoti_torch_get_strides(
+    AtenTensorHandle tensor,
+    int64_t** ret_strides) {
     std::string msg = std::string(__func__) + " DNE";
     perror(msg.c_str());
   }
-  void aoti_torch_create_tensor_from_blob_v2() {
+  AOTITorchError aoti_torch_create_tensor_from_blob_v2(
+    void* data,
+    int64_t ndim,
+    const int64_t* sizes_ptr,
+    const int64_t* strides_ptr,
+    int64_t storage_offset,
+    int32_t dtype,
+    int32_t device_type,
+    int32_t device_index,
+    AtenTensorHandle* ret_new_tensor,
+    int32_t layout,
+    const uint8_t* opaque_metadata,
+    int64_t opaque_metadata_size) {
     std::string msg = std::string(__func__) + " DNE";
     perror(msg.c_str());
   }
-  void aoti_torch_delete_cuda_stream_guard() {
+  AOTITorchError aoti_torch_delete_cuda_stream_guard(
+    CUDAStreamGuardHandle guard) {
     std::string msg = std::string(__func__) + " DNE";
     perror(msg.c_str());
   }
   int aoti_torch_device_type_cpu() {
     return 0;
   }
-  void aoti_torch_delete_tensor_object() {
+  AOTITorchError aoti_torch_delete_tensor_object(AtenTensorHandle tensor) {
     std::string msg = std::string(__func__) + " DNE";
     perror(msg.c_str());
   }
-  void aoti_torch_get_storage_size() {
+  AOTITorchError aoti_torch_get_storage_size(
+    AtenTensorHandle tensor,
+    int64_t* ret_size) {
     std::string msg = std::string(__func__) + " DNE";
     perror(msg.c_str());
   }
-  void aoti_torch_create_cuda_stream_guard() {
+  AOTITorchError aoti_torch_create_cuda_stream_guard(
+    void* stream,
+    int32_t device_index,
+    CUDAStreamGuardHandle* ret_guard) {
     std::string msg = std::string(__func__) + " DNE";
     perror(msg.c_str());
   }
@@ -91,7 +113,14 @@ extern "C" {
   int32_t aoti_torch_dtype_float32() {
     return 6;
   }
-  void aoti_torch_empty_strided() {
+  AOTITorchError aoti_torch_empty_strided(
+    int64_t ndim,
+    const int64_t* sizes_ptr,
+    const int64_t* strides_ptr,
+    int32_t dtype,
+    int32_t device_type,
+    int32_t device_index,
+    AtenTensorHandle* ret_new_tensor) {
     std::string msg = std::string(__func__) + " DNE";
     perror(msg.c_str());
   }
@@ -118,6 +147,9 @@ int main() {
 
   AtenTensorHandle* inputs;
   AtenTensorHandle* outputs;
+
+  
+
 
   return 0;
 }
