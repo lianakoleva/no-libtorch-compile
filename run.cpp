ce79f5a7a0905655287177cb5b97d63ea22395f7
albanD <desmaison.alban@gmail.com>
2024-09-21 20:12:17 -0400

    more

--- run.cpp
+++ run.cpp
@@ -5,6 +5,27 @@
 #include <iostream>
 
 extern "C" {
+  struct AtenTensorHandle {
+    void* data_ptr;
+  };
+
+
+  using AOTIRuntimeError = int32_t;
+  using AOTITorchError = int32_t;
+
+  struct AOTInductorModelContainerOpaque;
+  using AOTInductorModelContainerHandle = AOTInductorModelContainerOpaque*;
+
+  AOTIRuntimeError AOTInductorModelContainerCreateWithDevice(
+    AOTInductorModelContainerHandle* container_handle,
+    size_t num_models,
+    const char* device_str,
+    const char* cubin_dir);
+
+  AOTIRuntimeError AOTInductorModelContainerGetNumInputs(
+    AOTInductorModelContainerHandle container_handle,
+    size_t* num_constants);
+
   int32_t aoti_torch_device_type_cuda() {
     return 1;
   }
@@ -14,10 +35,12 @@ extern "C" {
   void aoti_torch_grad_mode_set_enabled(bool enabled) {
     std::string msg = std::string(__func__) + " DNE";
     perror(msg.c_str());
+    return 0;
   }
-  void aoti_torch_get_data_ptr() {
-    std::string msg = std::string(__func__) + " DNE";
-    perror(msg.c_str());
+  AOTITorchError aoti_torch_get_data_ptr(
+    AtenTensorHandle tensor,
+    void** ret_data_ptr) {
+    *ret_data_ptr = tensor.data_ptr;
   }
   void aoti_torch_get_storage_offset() {
     std::string msg = std::string(__func__) + " DNE";
@@ -50,9 +73,20 @@ extern "C" {
     std::string msg = std::string(__func__) + " DNE";
     perror(msg.c_str());
   }
-  void aoti_torch_create_tensor_from_blob() {
-    std::string msg = std::string(__func__) + " DNE";
-    perror(msg.c_str());
+  AOTITorchError aoti_torch_create_tensor_from_blob(
+    void* data,
+    int64_t ndim,
+    const int64_t* sizes_ptr,
+    const int64_t* strides_ptr,
+    int64_t storage_offset,
+    int32_t dtype,
+    int32_t device_type,
+    int32_t device_index,
+    AtenTensorHandle* ret_new_tensor)
+  {
+    ret_new_tensor = new AtenTensorHandle();
+    ret_new_tensor->data_ptr = (void*)1234;
+    return 0;
   }
   int32_t aoti_torch_dtype_float32() {
     return 6;
@@ -61,20 +95,6 @@ extern "C" {
     std::string msg = std::string(__func__) + " DNE";
     perror(msg.c_str());
   }
-
-  using AOTIRuntimeError = int32_t;
-
-  struct AOTInductorModelContainerOpaque;
-  using AOTInductorModelContainerHandle = AOTInductorModelContainerOpaque*;
-
-  AOTIRuntimeError AOTInductorModelContainerCreateWithDevice(
-    AOTInductorModelContainerHandle* container_handle,
-    size_t num_models,
-    const char* device_str,
-    const char* cubin_dir);
-  AOTIRuntimeError AOTInductorModelContainerGetNumInputs(
-    AOTInductorModelContainerHandle container_handle,
-    size_t* ret_num_inputs);
 }
 
 int main() {
@@ -88,9 +108,16 @@ int main() {
     "cuda",
     nullptr);
   printf("container_handle=%p\n", container_handle_);
-  size_t num_inputs = 33333;
-  err = AOTInductorModelContainerGetNumInputs(container_handle_, &num_inputs);
-  printf("num_inputs=%lu\n", num_inputs);
+
+  size_t num_inputs;
+  AOTInductorModelContainerGetNumInputs(
+    container_handle_,
+    &num_inputs);
+  std::cout<<num_inputs<<std::endl;
+
+
+  AtenTensorHandle* inputs;
+  AtenTensorHandle* outputs;
 
   return 0;
 }
